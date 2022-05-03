96b797dc3a17d80dae05e3217f91866875a269e5
nimo <>
2022-05-03 23:43:08 +0800

    更新 ＣＵＲＤ

--- routes/posts.js
+++ routes/posts.js
@@ -2,6 +2,7 @@
 var express = require("express");
 var router = express.Router();
 const Post = require("../models/postsModel");
+const handleError = require("../service/handleError");
 //資料全撈
 router.get("/", async (req, res) => {
   //res.send("respond with a resource"); //回傳網頁格式
@@ -12,60 +13,61 @@ router.get("/", async (req, res) => {
 });
 //新增單筆
 router.post("/", async (req, res) => {
-  const { body } = req;
-  const newpost = await Post.create(body);
-  if (body.content !== "" && body.content !== undefined) {
-    res.status(200).json({
-      status: "status",
-      post: newpost
-    });
-  } else {
-    res.status(400).json({
-      status: "false",
-      data: "使用者資料未填寫完整"
-    });
+  try {
+    const { body } = req;
+    const newpost = await Post.create(body);
+    if (body.content) {
+      res.status(200).json({
+        status: "status",
+        post: newpost
+      });
+    } else if (body.name) {
+      res.status(200).json({
+        status: "status",
+        post: newpost
+      });
+    } else {
+      handleError(res);
+    }
+  } catch (err) {
+    handleError(res, err);
   }
 });
 //編輯單筆
 router.patch("/:id", async (req, res) => {
   const { body } = req;
   const { id } = req.params;
-  if (body.content !== "" && body.content !== undefined) {
-    const editPost = await Post.findByIdAndUpdate(id, body);
-    if (editPost !== null) {
-      res.status(200).json({
-        status: "success",
-        data: await Post.findById(editPost._id)
-      });
+  const editPost = await Post.findByIdAndUpdate(id, body);
+  console.log("iddd", editPost._id);
+  try {
+    if (body.content) {
+      const editPost = await Post.findByIdAndUpdate(id, body);
+      if (editPost) {
+        res.status(200).json({
+          status: "success",
+          data: await Post.findById(editPost._id)
+        });
+      } else {
+        handleError(res);
+      }
     } else {
-      res.status(400).json({
-        status: "false",
-        data: "使用者資料未填寫完整"
-      });
+      handleError(res);
     }
-  } else {
-    res.status(400).json({
-      status: "false",
-      methods: "patch",
-      data: "使用者資料未填寫完整"
-    });
+  } catch (err) {
+    handleError(res, err);
   }
 });
 //刪除單筆
 router.delete("/:id", async (req, res) => {
   const { id } = req.params;
   const delPost = await Post.findByIdAndDelete(id);
-  if (delPost !== null) {
+  if (delPost) {
     res.status(200).json({
       status: "success",
       data: await Post.find()
     });
   } else {
-    res.status(400).json({
-      status: "false",
-      methods: "del",
-      data: "id不正確"
-    });
+    handleError(res);
   }
 });
 module.exports = router;
