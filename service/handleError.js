1b179dff33db73a57e292487889153f6d5ea6458
nimo <>
2022-05-18 14:38:27 +0800

    [改寫] fn加入next參數

--- service/handleError.js
+++ service/handleError.js
@@ -1,14 +1,7 @@
-const handleError = (res, err) => {
-  let message = "";
-  if (err) {
-    message = err.message;
-  } else {
-    message = "欄位未填寫正確或無此 id";
-  }
-  res.status(400).send({
-    status: false,
-    message
-  });
+const handleError = (httpStatus, errMessage, next) => {
+  const err = new Error(errMessage);
+  err.statusCode = httpStatus;
+  err.isOperational = true;
+  next(err);
 };
-
 module.exports = handleError;
